707ad0275a2e3031e73a1d44ae3fdacf27844054
KWY <wodrk1001@gmail.com>
2024-08-08 17:22:26 +0900

    #2 [feat] 도메인 객체 구현

---
+++ coupon/src/main/kotlin/org/kwy/coupon/coupon/domain/Coupon.kt
@@ -0,0 +1,18 @@
+package org.kwy.coupon.coupon.domain
+
+import org.kwy.coupon.coupon.domain.policy.DiscountPolicy
+import java.time.LocalDate
+
+data class Coupon(
+    val id: Long? = null,
+    val name: String,
+    val startDate: LocalDate,
+    val endDate: LocalDate,
+    val maxIssuePerUserCnt: Int,
+    val totalIssue: Int,
+    val discountType: DiscountType,
+    val discountValue: Int,
+    val maxDiscountAmount: Int? = null,
+    val type: CouponType,
+    val targets: List<Long>?
+)
---
+++ coupon/src/main/kotlin/org/kwy/coupon/coupon/domain/DiscountType.kt
@@ -0,0 +1,5 @@
+package org.kwy.coupon.coupon.domain
+
+enum class DiscountType {
+    AMOUNT, PERCENTAGE
+}
